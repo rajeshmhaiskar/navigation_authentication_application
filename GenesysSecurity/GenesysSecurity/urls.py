3241c24975895569aca2af56b51a451d7d05c516
rajeshmhaiskar <141298570+rajeshmhaiskar@users.noreply.github.com>
2024-03-12 11:36:37 +0530

    Update urls.py

--- GenesysSecurity/GenesysSecurity/urls.py
+++ GenesysSecurity/GenesysSecurity/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +10,6 @@ urlpatterns = [
     path('main/', include('GenesysAuthenticator.urls'))
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
